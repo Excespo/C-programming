df347bebc87a1082e466276814cd381eb614aa17
Excespo <excespo@qq.com>
2021-09-22 13:26:49 +0800

    'calendar'

--- c_lecture/Week2/task4.c
+++ c_lecture/Week2/task4.c
@@ -2,19 +2,94 @@
 #include<stdlib.h>
 #include<string.h>
 
+int *get_date(char *);
+
+int leap_year(int);
+
+int days_since_ad(int *);
+
+int calendar(int);
+
 int main(int argc, char **argv){
-    char *date = argv[1];
-    printf("%s\n",date);
-    int datelist[] = {0,0,0};
+    char *date_str = argv[1];
+    int *year_month_day = get_date(date_str);
+    int days = days_since_ad(year_month_day);
+    // for(int yr = 1880; yr<2025; yr+=4){
+    //     printf("year %d is rn: %d\n",yr, leap_year(yr));
+    // }
+    printf("%d\n",calendar(days));
+    return 0;
+}
+
+int *get_date(char *date_str){
+    int *year_month_day = (int*)malloc(3*sizeof(int));
     char *seg = "-";
-    char* substr = strtok(date,seg);
+    char *substr = strtok(date_str,seg);
     int i = 0;
-    while(substr!=NULL){
-        datelist[i] = i;
+    while(substr){
+        *(year_month_day+i) = atoi(substr);
         i++;
-        printf("%d, %s, %d\n",i, substr, datelist[i]);
         substr = strtok(NULL,seg);
     }
-    // printf("%s\n",data);
-    return 0;
+    return year_month_day;
+}
+
+// what's the exact rule of leap year
+int leap_year(int year){
+    if((year%400==0)||(year%4==0&&year%100!=0)){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int days_since_ad(int *year_month_day){
+    int _days_since_ad = -1; // begin at 0001-01-01
+    int year = year_month_day[0];
+    int month = year_month_day[1];
+    int day = year_month_day[2];
+
+    // days in complete years
+    for(int yr=1;yr<year;yr++){
+        int days_1yr = 0;
+        days_1yr = (leap_year(yr))?366:365;
+        _days_since_ad += days_1yr;
+    }
+    // days in complete months
+    for(int m=1;m<month;m++){
+        int days_1m = 0;
+        if(m%2==1){
+            days_1m = 31;
+        }else if(m%2==0 && m!=2){
+            days_1m = 30;
+        }else{
+            days_1m = (leap_year(year))?29:28;
+        }
+        _days_since_ad += days_1m;
+    }
+    // days in current year and month
+    _days_since_ad += day;
+    
+    return _days_since_ad;
+}
+
+int calendar(int days){
+    // // compare with 0001-01-01, which is 7
+    // if(days_since_ad%7==0){
+    //     return 7;
+    // }else{
+    //     return days_since_ad%7;
+    // }
+
+    // compare with 2000-11-12, which is 7
+    
+    int origin[3] = {2000,11,12}; 
+    // int origin[3] = {1986,6,1}; 
+    int diff = days - days_since_ad(origin);
+
+    if(diff%7==0){
+        return 7;
+    }else{
+        return diff%7;
+    }
 }
