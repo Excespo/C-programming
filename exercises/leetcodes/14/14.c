415e4ce815539da6457b5d2b125e51ef92e2386f
Excespo <excespo@qq.com>
2021-09-26 11:47:02 +0800

    '.gitignore add tempCodeRunnerFile.c'

--- exercises/leetcodes/14/14.c
+++ exercises/leetcodes/14/14.c
@@ -8,18 +8,13 @@ char * longestCommonPrefix(char ** strs, int strsSize){
     }else if(strsSize==1){
         return strs[0];
     }else{
-        // printf("%d\n",strsSize);
         int str_len = strlen(strs[0]);
         int DO = 1;
         int finish_if=1;
         while(DO){
-            // printf("%d\n",str_len);
             finish_if = 1;
             for(int i=1;i<strsSize;i++){
-                // printf("for loop\n");
-                // printf("%s, %s, %d\n",strs[0],strs[i],memcmp(strs[0],strs[i],str_len));
                 if(memcmp(strs[0],strs[i],str_len)){ // if not the same
-                    // printf("if");
                     str_len--;
                     finish_if = 0;
                     break;
@@ -27,16 +22,18 @@ char * longestCommonPrefix(char ** strs, int strsSize){
             }
             if(finish_if) DO=0;
         }
-        char *common = (char*)malloc(strlen(strs[0])*sizeof(char));
+        char *common = (char*)malloc((str_len+1)*sizeof(char));
         memcpy(common,strs[0],str_len);
+        common[str_len] = '\0';
         return common;
     }
 }
 
-void main(){
-    char *_strings[] = {"dog", "racecar", "car"};
+int main(void)){
+    char *_strings[] = {"flow", "flower", "flight","flire"};
     char **strings = _strings;
     int stringsSize = sizeof(_strings)/sizeof(char*);
     printf("%d\n",stringsSize);
     printf("Longest common prefix is: %s, is null: %d\n", longestCommonPrefix(strings, stringsSize), longestCommonPrefix(strings, stringsSize)=="");
+    return 0;
 }
